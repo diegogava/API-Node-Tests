1c53197feb4d9fb433ccc1eba35ddea7cac2c96c
Diego Almeida Gava <diego.gava@mundiale.com.br>
2020-06-23 11:01:38 -0300

    Changing the organization of folders and files used in tests.

--- __tests__/units/aluno.test.js
+++ __tests__/units/aluno.test.js
@@ -2,7 +2,8 @@ const chai = require('chai');
 const http = require('chai-http'); // Extensão da lib chai p/ simular requisições http
 const subSet = require('chai-subset'); // Extensao da lib chai p/ verificar objetos
 
-const index = require('../index'); // Arquivo a ser testado
+// Arquivo a ser testado
+const index = require('../../index');
 
 chai.use(http);
 chai.use(subSet);
@@ -14,21 +15,16 @@ const alunoSchema = {
     sala: sala => sala
 };
 
-describe('Teste das funcoes', () => {
-
+describe('Teste das funções', () => {
     it('addAluno', () => {
         const aluno = index.addAluno('matheus', 'sala 1');
-
         // Verifica se as caracteristicas do objeto aluno é igual ao alunoSchema
         chai.expect(aluno).to.containSubset(alunoSchema);
     });
-
     it('getAlunos', () => {
-
         index.addAluno('osmar', 'sala 1');
         index.addAluno('mariana', 'sala 2');
         const alunos = index.getAlunos();
-        
         chai.expect(alunos.length).to.be.equals(3);
         // Primeiro se verifica se está retornando um array
         // Verifica se as caracteristicas dos objetos no array é igual ao alunoSchema
@@ -37,7 +33,6 @@ describe('Teste das funcoes', () => {
 });
 
 describe('Testes de integração', () => {
-
     it('/aluno - POST', () => {
         chai.request(index.app)
             .post('/aluno')
@@ -47,17 +42,16 @@ describe('Testes de integração', () => {
             })
             .end((err, res) => {
                 chai.expect(err).to.be.null;
-                chai.expect(res).to.have.status(201); 
+                chai.expect(res).to.have.status(201);
                 chai.expect(res.body).to.containSubset(alunoSchema);
             });
     });
-
     it('/aluno - GET', () => {
         chai.request(index.app)
             .get('/aluno')
             .end((err, res) => {
                 chai.expect(err).to.be.null;
-                chai.expect(res).to.have.status(200); 
+                chai.expect(res).to.have.status(200);
                 chai.expect(res.body.length).to.be.equal(4);
                 chai.expect(res.body).to.containSubset([alunoSchema]);
             });
